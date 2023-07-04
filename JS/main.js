c4c2534846761822b4dfb92fb5f20bdba8ad63c4
MyUsername <my@email.com>
2023-07-04 17:15:16 +0200

    Commit push from 17.15

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,11 @@
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0) {
-        console.log("Fizz");
+        console.log(i)
+    if (i % 3 == 0 && i % 5 ==0) {
+        console.log("FizzBuzz");
     } else if ( i % 5 == 0) {
         console.log("Buzz");
-    } else if (i % 3 == 0 && i % 5 ==0) {
-        console.log("FizzBuzz");
-    } else {
-        console.log (i);
-    }
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+    } 
 
 };
